c9c0864da3bef76feeb05d79a7abbe25cc16ca09
filippo-tiozzo <887683@stud.unive.it>
2024-08-24 18:10:33 +0200

    Correzione grant

--- website/grant.sql
+++ website/grant.sql
@@ -4,14 +4,13 @@ GRANT ALL PRIVILEGES ON SEQUENCE utenti_id_seq TO amministratore;
 GRANT ALL PRIVILEGES ON SEQUENCE acquirente_id_seq TO amministratore;
 GRANT ALL PRIVILEGES ON SEQUENCE venditore_id_seq TO amministratore;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON Prodotto TO Venditore;
-GRANT SELECT, UPDATE(spedito) ON OrdineProdotto TO Venditore;
-GRANT SELECT, UPDATE(stato) ON Ordine TO Venditore;
+GRANT SELECT, INSERT, UPDATE, DELETE ON prodotto TO Venditore;
+GRANT SELECT, UPDATE(spedito) ON ordine_prodotto TO Venditore;
+GRANT SELECT, UPDATE(stato) ON ordine TO Venditore;
 
-GRANT SELECT, UPDATE(quantita) ON Prodotto TO Acquirente;
-GRANT SELECT, DELETE ON Carrello TO Acquirente;
-GRANT SELECT, INSERT, UPDATE, DELETE ON CarrelloProdotto TO Acquirente;
-GRANT SELECT, INSERT  ON Ordine TO Acquirente;
-GRANT SELECT, INSERT,  ON Ordine TO Acquirente;
-GRANT SELECT, INSERT ON OrdineProdotto TO Acquirente;
-GRANT SELECT, INSERT, UPDATE ON Recensione TO Acquirente;
+GRANT SELECT, UPDATE(quantita) ON prodotto TO Acquirente;
+GRANT SELECT, DELETE ON carrello TO Acquirente;
+GRANT SELECT, INSERT, UPDATE, DELETE ON carrello_prodotto TO Acquirente;
+GRANT SELECT, INSERT  ON ordine TO Acquirente;
+GRANT SELECT, INSERT ON ordine_prodotto TO Acquirente;
+GRANT SELECT, INSERT, UPDATE ON recensione TO Acquirente;
