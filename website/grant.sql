aa5680a685c54d8f378fc44316620e79d9400423
filippo-tiozzo <887683@stud.unive.it>
2024-08-24 18:32:13 +0200

    Aggiunta grant della password

--- website/grant.sql
+++ website/grant.sql
@@ -7,6 +7,7 @@ GRANT ALL PRIVILEGES ON SEQUENCE venditore_id_seq TO amministratore;
 GRANT SELECT, INSERT, UPDATE, DELETE ON prodotto TO venditore;
 GRANT SELECT, UPDATE(spedito) ON ordine_prodotto TO venditore;
 GRANT SELECT, UPDATE(stato) ON ordine TO venditore;
+GRANT SELECT; UPDATE(password) ON utenti TO venditore;
 
 GRANT SELECT, UPDATE(quantita) ON prodotto TO acquirente;
 GRANT SELECT, DELETE ON carrello TO acquirente;
@@ -14,3 +15,4 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON carrello_prodotto TO acquirente;
 GRANT SELECT, INSERT  ON ordine TO acquirente;
 GRANT SELECT, INSERT ON ordine_prodotto TO acquirente;
 GRANT SELECT, INSERT, UPDATE ON recensione TO acquirente;
+GRANT SELECT; UPDATE(password) ON utenti TO acquirente;
