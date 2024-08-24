8af402acc25f575c640df2e31c347b30e7bef35f
filippo-tiozzo <887683@stud.unive.it>
2024-08-24 18:15:30 +0200

    Correzione coerenza con database

--- website/grant.sql
+++ website/grant.sql
@@ -4,13 +4,13 @@ GRANT ALL PRIVILEGES ON SEQUENCE utenti_id_seq TO amministratore;
 GRANT ALL PRIVILEGES ON SEQUENCE acquirente_id_seq TO amministratore;
 GRANT ALL PRIVILEGES ON SEQUENCE venditore_id_seq TO amministratore;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON prodotto TO Venditore;
-GRANT SELECT, UPDATE(spedito) ON ordine_prodotto TO Venditore;
-GRANT SELECT, UPDATE(stato) ON ordine TO Venditore;
+GRANT SELECT, INSERT, UPDATE, DELETE ON prodotto TO venditore;
+GRANT SELECT, UPDATE(spedito) ON ordine_prodotto TO venditore;
+GRANT SELECT, UPDATE(stato) ON ordine TO venditore;
 
-GRANT SELECT, UPDATE(quantita) ON prodotto TO Acquirente;
-GRANT SELECT, DELETE ON carrello TO Acquirente;
-GRANT SELECT, INSERT, UPDATE, DELETE ON carrello_prodotto TO Acquirente;
-GRANT SELECT, INSERT  ON ordine TO Acquirente;
-GRANT SELECT, INSERT ON ordine_prodotto TO Acquirente;
-GRANT SELECT, INSERT, UPDATE ON recensione TO Acquirente;
+GRANT SELECT, UPDATE(quantita) ON prodotto TO acquirente;
+GRANT SELECT, DELETE ON carrello TO acquirente;
+GRANT SELECT, INSERT, UPDATE, DELETE ON carrello_prodotto TO acquirente;
+GRANT SELECT, INSERT  ON ordine TO acquirente;
+GRANT SELECT, INSERT ON ordine_prodotto TO acquirente;
+GRANT SELECT, INSERT, UPDATE ON recensione TO acquirente;
